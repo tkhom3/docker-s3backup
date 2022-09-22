dff1a6266385f7705294b71a31c4ffd941bc26a9
Kieffer Todd <KiefferToddM@JohnDeere.com>
2022-09-22 09:44:50 -0500

    fix codacity errors

--- README.md
+++ README.md
@@ -13,10 +13,13 @@ Mount a second directory to `/config` for the cache file.
 
 ## Environment Variables
 
-| **Variable**  | **Default** | **Description**                             |
-|---------------|-------------|---------------------------------------------|
-| ACCESS_KEY    |             | AWS IAM Access Key                          |
-| SECRET_KEY    |             | AWS IAM Secret Key                          |
-| S3PATH        |             | S3 bucket and path                          |
-| S3CMDPARAMS   |             | Custom S3cmd parameters                     |
-| CRON_SCHEDULE | `0 3 * * 6` | How often a backup should be run using CRON |
+| **Variable**  | **Default**          | **Description**                             |
+|---------------|----------------------|---------------------------------------------|
+| ACCESS_KEY    |                      | AWS IAM Access Key                          |
+| SECRET_KEY    |                      | AWS IAM Secret Key                          |
+| S3PATH        |                      | S3 bucket and path                          |
+| S3CMDPARAMS   |                      | Custom S3cmd parameters                     |
+| CRON_SCHEDULE |`0 3 * * 6`           | How often a backup should be run using CRON |
+| LOG_LEVEL     |`INFO`                | Logging Level                               |
+| CACHE_FILE    |`/tmp/s3cmd_cache.txt`| Location to write the file cache            |
+| LOG_FILE      |`/tmp/s3backup.log`   | Location to write the log file              |
--- run.sh
+++ run.sh
@@ -6,23 +6,24 @@ ACCESS_KEY=${ACCESS_KEY:?"ACCESS_KEY required"}
 SECRET_KEY=${SECRET_KEY:?"SECRET_KEY required"}
 S3PATH=${S3PATH:?"S3_PATH required"}
 CRON_SCHEDULE=${CRON_SCHEDULE:-0 3 * * 6}
+LOG_LEVEL=${LOG_LEVEL:INFO}
+CACHE_FILE=${CACHE_FILE:/tmp/s3cmd_cache.txt}
+LOG_FILE=${LOG_FILE:/tmp/s3backup.log}
 
 LOCKFILE="$HOME/s3cmd.lock"
-LOG="/config/s3backup.log"
-CACHE="/config/s3cmd_cache.txt"
 
 # Cleanup file on exit
-trap "rm -f "$LOCKFILE"" EXIT
+trap 'rm -f "$LOCKFILE"' EXIT
 
-if [ ! -e $LOG ]; then
-  touch $LOG
+if [ ! -e "$LOG_FILE" ]; then
+  touch "$LOG_FILE"
 fi
 
-if [ ! -e $CACHE ]; then
-  touch $CACHE
+if [ ! -e "$CACHE_FILE" ]; then
+  touch "$CACHE_FILE"
 fi
 
-if [[ $OPTION = "start" ]]; then
+if [[ "$OPTION" = "start" ]]; then
   echo "Found the following files to sync:"
   echo
   ls -F /backup
@@ -40,25 +41,37 @@ if [[ $OPTION = "start" ]]; then
     sed -i 's@secret_key =@secret_key = '"$SECRET_KEY"'@g' "$HOME/s3cmd.cfg"
   fi
 
-  echo "Running backup on the following CRON schedule: $CRON_SCHEDULE"
-  echo "$CRON_SCHEDULE sh /run.sh backup" | crontab - && crond -f -L /dev/stdout
+  if grep -Fq "$CACHE_FILE" "$HOME/s3cmd.cfg"; then
+    echo "CACHE_FILE already exists in $HOME/s3cmd.cfg"
+  else
+    sed -i 's@cache_file =@cache_file = '"$CACHE_FILE"'@g' "$HOME/s3cmd.cfg"
+  fi
 
-elif [[ $OPTION = "backup" ]]; then
-  echo "Starting sync: $(date)" | tee $LOG
+  if grep -Fq "verbosity = $LOG_LEVEL" "$HOME/s3cmd.cfg"; then
+    echo "LOG_LEVEL already exists in $HOME/s3cmd.cfg"
+  else
+    sed -i 's@verbosity =@verbosity = '"$LOG_LEVEL"'@g' "$HOME/s3cmd.cfg"
+  fi
 
-  if [ -f $LOCKFILE ]; then
-    echo "$LOCKFILE detected, exiting! Already running?" | tee -a $LOG
+  echo 'Running backup on the following CRON schedule: "$CRON_SCHEDULE"'
+  echo '"$CRON_SCHEDULE" sh "$HOME/run.sh" "$HOME" backup' | crontab - && crond -f -L /dev/stdout
+
+elif [[ "$OPTION" = "backup" ]]; then
+  echo "Starting sync: $(date)" | tee "$LOG_FILE"
+
+  if [ -f "$LOCKFILE" ]; then
+    echo '"$LOCKFILE" detected, exiting! Already running?' | tee -a "$LOG_FILE"
     exit 1
   else
     touch "$LOCKFILE"
   fi
 
-  echo "Executing s3cmd sync -c $HOME/s3cmd.cfg $S3CMDPARAMS /backup/ $S3PATH" | tee -a $LOG
-  s3cmd sync -c "$HOME/s3cmd.cfg" "$S3CMDPARAMS" /backup/ "$S3PATH" 2>&1 | tee -a $LOG
+  echo 'Executing s3cmd sync -c "$HOME/s3cmd.cfg" "$S3CMDPARAMS" /backup/ "$S3PATH"' | tee -a "$LOG_FILE"
+  s3cmd sync -c "$HOME/s3cmd.cfg" "$S3CMDPARAMS" /backup/ "$S3PATH" 2>&1 | tee -a "$LOG_FILE"
   rm -f "$LOCKFILE"
-  echo "Finished sync: $(date)" | tee -a $LOG
+  echo "Finished sync: $(date)" | tee -a "$LOG_FILE"
 
 else
-  echo "Unsupported option: $OPTION" | tee -a $LOG
+  echo 'Unsupported option: "$OPTION"' | tee -a "$LOG_FILE"
   exit 1
 fi
