ddf50dcc84254df0eab076b6a8648e45ea1a7cdf
Todd Kieffer <ToddMKieffer@gmail.com>
2025-01-05 15:35:45 -0600

    moved to a dedicated user and supercronic

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
-FROM python:3.12.8-slim
+FROM python:3.12.8-alpine3.21
 
-ARG USER=root
-ARG GROUP=root
+ARG USER=s3backup
+ARG GROUP=s3backup
 
 ENV APP_DIR="/s3backup"
 ENV BACKUP_DIR="/backup"
@@ -13,21 +13,28 @@ ENV LOG_LEVEL="INFO"
 ENV CACHE_FILE="s3cmd_cache.txt"
 ENV LOG_FILE="s3backup.log"
 
+RUN apk update && apk add --no-cache \
+    supercronic \
+    shadow
+
+RUN useradd -m $USER
+
 RUN mkdir $APP_DIR $BACKUP_DIR && \
     chown $USER:$GROUP $APP_DIR $BACKUP_DIR && \
-    chmod 400 $BACKUP_DIR && \
-    chmod 600 $APP_DIR
+    chmod 440 $BACKUP_DIR && \
+    chmod 770 $APP_DIR
 
 WORKDIR $APP_DIR
 
 COPY --chown=$USER:$GROUP s3cmd.cfg .
 COPY --chown=$USER:$GROUP run.sh .
 
-RUN chmod 500 run.sh && \
-    chmod 600 s3cmd.cfg
+RUN chmod 554 run.sh && \
+    chmod 664 s3cmd.cfg
 
 COPY requirements.txt /tmp/
 RUN pip install --requirement /tmp/requirements.txt
 
+USER $USER
+
 ENTRYPOINT ["sh", "run.sh"]
-CMD ["start"]
--- run.sh
+++ run.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-OPTION="$1"
-LOCKFILE="/tmp/s3cmd.lock"
-
-# Cleanup file on exit
-trap 'rm -f "$LOCKFILE"' EXIT
-
 if [ ! -e "$APP_DIR/$LOG_FILE" ]; then
   touch "$APP_DIR/$LOG_FILE"
 fi
@@ -14,56 +8,40 @@ if [ ! -e "$APP_DIR/$CACHE_FILE" ]; then
   touch "$APP_DIR/$CACHE_FILE"
 fi
 
-if [ "$OPTION" = "start" ]; then
-  echo "Found the following files to sync:"
-  echo
-  ls -F $BACKUP_DIR
-  echo
-
-  if grep -Fq "$ACCESS_KEY" "$APP_DIR/s3cmd.cfg"; then
-    echo "ACCESS_KEY already exists in $APP_DIR/s3cmd.cfg"
-  else
-    sed -i 's@access_key =@access_key = '"$ACCESS_KEY"'@g' "$APP_DIR/s3cmd.cfg"
-  fi
-  
-  if grep -Fq "$SECRET_KEY" "$APP_DIR/s3cmd.cfg"; then
-    echo "SECRET_KEY already exists in $APP_DIR/s3cmd.cfg"
-  else
-    sed -i 's@secret_key =@secret_key = '"$SECRET_KEY"'@g' "$APP_DIR/s3cmd.cfg"
-  fi
-
-  if grep -Fq "$APP_DIR/$CACHE_FILE" "$APP_DIR/s3cmd.cfg"; then
-    echo "CACHE_FILE already exists in $APP_DIR/s3cmd.cfg"
-  else
-    sed -i 's@cache_file =@cache_file = '"$APP_DIR/$CACHE_FILE"'@g' "$APP_DIR/s3cmd.cfg"
-  fi
-
-  if grep -Fq "verbosity = $LOG_LEVEL" "$APP_DIR/s3cmd.cfg"; then
-    echo "LOG_LEVEL already exists in $APP_DIR/s3cmd.cfg"
-  else
-    sed -i 's@verbosity =@verbosity = '"$LOG_LEVEL"'@g' "$APP_DIR/s3cmd.cfg"
-  fi
-
-
-  echo "Running backup on the following CRON schedule: $CRON_SCHEDULE"
-  echo "$CRON_SCHEDULE sh $APP_DIR/run.sh backup" | crontab - && crond -f -L /dev/stdout
-
-elif [ "$OPTION" = "backup" ]; then
-  echo "Starting sync: $(date)" | tee -a "$APP_DIR/$LOG_FILE"
-
-  if [ -f "$LOCKFILE" ]; then
-    echo "$LOCKFILE detected, exiting! Already running?" | tee -a "$APP_DIR/$LOG_FILE"
-    exit 1
-  else
-    touch "$LOCKFILE"
-  fi
-
-  echo "Executing s3cmd sync -c $APP_DIR/s3cmd.cfg $S3CMDPARAMS $BACKUP_DIR/ $S3PATH" | tee -a "$APP_DIR/$LOG_FILE"
-  s3cmd sync -c "$APP_DIR/s3cmd.cfg" "$S3CMDPARAMS" $BACKUP_DIR/ "$S3PATH" 2>&1 | tee -a "$APP_DIR/$LOG_FILE"
-  rm -f "$LOCKFILE"
-  echo "Finished sync: $(date)" | tee -a "$APP_DIR/$LOG_FILE"
-
-else
-  echo "Unsupported option: $OPTION" | tee -a "$APP_DIR/$LOG_FILE"
-  exit 1
+if [ -f "$APP_DIR/crontab" ]; then
+  rm "$APP_DIR/crontab"
 fi
+
+if grep -Fq "$ACCESS_KEY" "$APP_DIR/s3cmd.cfg"; then
+  echo "ACCESS_KEY already exists in $APP_DIR/s3cmd.cfg"
+else
+  sed -i 's@access_key =@access_key = '"$ACCESS_KEY"'@g' "$APP_DIR/s3cmd.cfg"
+fi
+
+if grep -Fq "$SECRET_KEY" "$APP_DIR/s3cmd.cfg"; then
+  echo "SECRET_KEY already exists in $APP_DIR/s3cmd.cfg"
+else
+  sed -i 's@secret_key =@secret_key = '"$SECRET_KEY"'@g' "$APP_DIR/s3cmd.cfg"
+fi
+
+if grep -Fq "$APP_DIR/$CACHE_FILE" "$APP_DIR/s3cmd.cfg"; then
+  echo "CACHE_FILE already exists in $APP_DIR/s3cmd.cfg"
+else
+  sed -i 's@cache_file =@cache_file = '"$APP_DIR/$CACHE_FILE"'@g' "$APP_DIR/s3cmd.cfg"
+fi
+
+if grep -Fq "verbosity = $LOG_LEVEL" "$APP_DIR/s3cmd.cfg"; then
+  echo "LOG_LEVEL already exists in $APP_DIR/s3cmd.cfg"
+else
+  sed -i 's@verbosity =@verbosity = '"$LOG_LEVEL"'@g' "$APP_DIR/s3cmd.cfg"
+fi
+
+echo
+echo "----------------------------------------"
+echo "Found the following directories to sync:"
+ls -F $BACKUP_DIR
+echo "----------------------------------------"
+
+echo "$CRON_SCHEDULE s3cmd sync -c $APP_DIR/s3cmd.cfg --cache-file=$APP_DIR/$CACHE_FILE $BACKUP_DIR/ $S3PATH" >> "$APP_DIR/crontab"
+# supercronic -split-logs -debug crontab 1>$APP_DIR/$LOG_FILE
+supercronic -debug crontab
